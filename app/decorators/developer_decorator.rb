5b44e9162a831d9d296e2087e04dc1a3a94722ae
piotrekpasciak <piotrek.pasciak@gmail.com>
2016-09-12 04:55:12 +0200

    Creation date for each developer with decorator test

--- app/decorators/developer_decorator.rb
+++ app/decorators/developer_decorator.rb
@@ -4,4 +4,12 @@ class DeveloperDecorator < Draper::Decorator
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def creation_date
+    day = created_at.day < 10 ? '0' + created_at.day.to_s : created_at.day.to_s
+    month = created_at.month < 10 ? '0' + created_at.month.to_s : created_at.month.to_s
+    year = created_at.year.to_s
+
+    day + '/' + month + '/' + year
+  end
 end
--- spec/decorators/developer_decorator_spec.rb
+++ spec/decorators/developer_decorator_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
-describe DeveloperDecorator do
-  let(:developer) { create :developer, first_name: 'Zbyszek', last_name: 'Zbyszkowski' }
+describe DeveloperDecorator, focus: true do
+  let(:developer) { create :developer, first_name: 'Zbyszek', last_name: 'Zbyszkowski',
+                           created_at: 'Mon June 1 12:03:08 -0700 2016' }
 
   describe '#full_name' do
     subject { developer.decorate.full_name }
     it { is_expected.to eq 'Zbyszek Zbyszkowski' }
+  end
+
+  describe '#creation_date' do
+    subject { developer.decorate.creation_date }
+    it { is_expected.to eq '01/06/2016'}
   end
 end
